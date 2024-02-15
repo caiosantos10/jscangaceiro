cd41f461f5fe96e0f9867247e5eefe0579fe39dd
Caio Santos <caiosant10@gmail.com>
2024-02-15 16:42:11 -0300

    listando negociacoes na tabela

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,24 +1,34 @@
- class NegociacaoController {
+class NegociacaoController {
     constructor() {
         let $ = document.querySelector.bind(document);
-
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event) {
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._negociacoes);
+        this._limpaFormulario();
+    }
 
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 0;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
         let data = DateConverter.paraData(this._inputData.value);
-
-        let negociacao = new Negociacao(
-            data, 
-            parseInt(this._inputQuantidade.value), 
+        return new Negociacao(
+            data,
+            parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-
-        console.log(negociacao);
-        console.log(DateConverter.paraTexto(negociacao.data));
     }
- }
+}
