90d34aec98950118187662c86dc704c5473f4503
Caio Santos <caiosant10@gmail.com>
2024-02-28 10:39:32 -0300

    utilizando ProxyFactory para criar instancia de Negociacoes que atualizacao a view

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,19 +5,9 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        const self = this;
-        this._negociacoes = new Proxy(new Negociacoes(), {
-            get(target, prop, receiver) {
-                if (typeof (target[prop]) == typeof (Function) && ['adiciona', 'esvazia'].includes(prop)) {
-                    return function () {
-                        console.log(`${prop} disparou a armadilha`);
-                        target[prop].apply(target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return target[prop];
-            }
-        });
+        this._negociacoes = ProxyFactory.create(
+            new Negociacoes(), ['adiciona', 'esvazia'], model => this._negociacoesView.update(model)
+        );
 
         this._negociacoesView = new NegociacoesView('#negociacoes');
         this._negociacoesView.update(this._negociacoes);
