f4a59467cb7eb94c0392c29f690a63de09e0c216
Caio Santos <caiosant10@gmail.com>
2024-02-29 16:19:11 -0300

    Criação de classe Bind para construir negociacoes e mensagens

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,29 +5,21 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._negociacoes = ProxyFactory.create(
-            new Negociacoes(), ['adiciona', 'esvazia'], model => this._negociacoesView.update(model)
-        );
-
-        this._negociacoesView = new NegociacoesView('#negociacoes');
-        this._negociacoesView.update(this._negociacoes);
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView('#mensagemView');
-        this._mensagemView.update(this._mensagem);
+        this._negociacoes = new Bind(new Negociacoes(), new NegociacoesView('#negociacoes'), ['adiciona', 'esvazia']);
+        
+        this._mensagem = new Bind(new Mensagem(), new MensagemView('#mensagensView'), ['texto']);
     }
 
     adiciona(event) {
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
 
     apaga() {
         this._negociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
 
     _limpaFormulario() {
